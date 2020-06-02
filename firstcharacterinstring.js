323c920900ee1f4fd8de2f923bc58797ee7b57bf
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-02 19:12:19 -0400

    solutions

---
+++ firstcharacterinstring.js
@@ -0,0 +1,28 @@
+//Objective is to find the first unique letter's index in a string
+//For example, "leetcode" would return 0
+//If not found, return -1
+
+let string = 'leetcode'
+
+
+//O(n) solution
+
+let map = {}
+
+//Easy counter to find the frequency of all characters in string
+for (let i = 0; i < string.length; i++) {
+    if (map[string.charAt(i)] === undefined) {
+        map[string.charAt(i)] = 1
+    } else {
+        map[string.charAt(i)]++
+    }
+}
+
+//Starting from the front of the string, find character with frequency of 1
+for (let i = 0; i < string.length; i++) {
+    if (map[string.charAt(i)] === 1) {
+        return i
+    }
+}
+
+return -1
