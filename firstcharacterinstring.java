e67e6637cd87663b8eb31398350d7ee2f04dfdb6
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-29 17:16:41 -0400

    added java solution

---
+++ firstcharacterinstring.java
@@ -0,0 +1,19 @@
+//Java Solution
+
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] charArr = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            charArr[c - 'a']++;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (charArr[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
